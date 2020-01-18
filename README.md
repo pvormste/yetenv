7a3aa00268f377551821c23cc08b637c5ec5caff
Patric Vormstein <pvormstein@googlemail.com>
2020-01-18 19:24:59 +0100

    add possibility to customize variable name

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 # yetenv
 
-`yetenv` is small util package which helps to determine on which environment the application is running. It reads the `ENVIRONMENT` variable.
+`yetenv` is small util package which helps to determine on which environment the application is running. 
+It reads the a specific environment variable (Default: `ENVIRONMENT`). See examples below.
 
 The `ENVIRONMENT` values are not case-sensitives.
 
@@ -20,14 +21,65 @@ go get -u github.com/pvormste/yetenv
 
 ## Usage
 
+### Using the defaults
+
+Shell:
 ```bash
 $ ENVIRONMENT="production" go run main.go   # yetenv.Production
 $ ENVIRONMENT="staging" go run main.go      # yetenv.Staging
 $ ENVIRONMENT="test" go run main.go         # yetenv.Develop
 ```
 
+Go Code:
 ```go
 environment := yetenv.GetEnvironment()
+
+switch environment {
+case yetenv.Production:
+    // Do something in production environment
+case yetenv.Staging:
+    // Do something in staging environment
+case yetenv.Develop:
+    // Do something in develop environment
+}
+```
+
+### Changing the default variable name
+
+Shell:
+```bash
+$ APP_ENV="production" go run main.go   # yetenv.Production
+$ APP_ENV="staging" go run main.go      # yetenv.Staging
+$ APP_ENV="test" go run main.go         # yetenv.Develop
+```
+
+Go Code:
+```go
+yetenv.DefaultVariableName = "APP_ENV"
+environment := yetenv.GetEnvironment()
+
+switch environment {
+case yetenv.Production:
+    // Do something in production environment
+case yetenv.Staging:
+    // Do something in staging environment
+case yetenv.Develop:
+    // Do something in develop environment
+}
+```
+
+### Reading temporary from a custom variable name
+
+Shell:
+```bash
+$ APP_ENV="production" go run main.go   # yetenv.Production
+$ APP_ENV="staging" go run main.go      # yetenv.Staging
+$ APP_ENV="test" go run main.go         # yetenv.Develop
+```
+
+Go Code:
+```go
+environment := yetenv.GetEnvironmentFromVariable("APP_ENV")
 
 switch environment {
 case yetenv.Production:
--- env_test.go
+++ env_test.go
@@ -73,4 +73,95 @@ func TestGetEnvironment(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("should return environemnt 'production' when changing DefaultVariableName", func(t *testing.T) {
+		values := []string{
+			"production",
+			"PRODUCTION",
+		}
+
+		DefaultVariableName = "CHANGED_ENV"
+
+		for _, value := range values {
+			value := value
+
+			t.Run(fmt.Sprintf("CHANGED_ENV=%s", value), func(t *testing.T) {
+				assert := assert.New(t)
+
+				err := os.Setenv("CHANGED_ENV", value)
+				require.NoError(t, err)
+
+				actualEnv := GetEnvironment()
+				assert.Equal(Production, actualEnv)
+			})
+		}
+	})
+}
+
+func TestGetEnvironmentFromVariable(t *testing.T) {
+	customVariableName := "CUSTOM_ENV"
+
+	t.Run("should return environment 'production' from variable 'CUSTOM_ENV'", func(t *testing.T) {
+		values := []string{
+			"PRODUCTION",
+			"production",
+		}
+
+		for _, value := range values {
+			value := value
+
+			t.Run(fmt.Sprintf("%s=%s", customVariableName, value), func(t *testing.T) {
+				assert := assert.New(t)
+
+				err := os.Setenv(customVariableName, value)
+				require.NoError(t, err)
+
+				actualEnv := GetEnvironmentFromVariable(customVariableName)
+				assert.Equal(Production, actualEnv)
+			})
+		}
+	})
+
+	t.Run("should return environment 'staging' from variable 'CUSTOM_ENV'", func(t *testing.T) {
+		values := []string{
+			"STAGING",
+			"staging",
+		}
+
+		for _, value := range values {
+			value := value
+
+			t.Run(fmt.Sprintf("%s=%s", customVariableName, value), func(t *testing.T) {
+				assert := assert.New(t)
+
+				err := os.Setenv(customVariableName, value)
+				require.NoError(t, err)
+
+				actualEnv := GetEnvironmentFromVariable(customVariableName)
+				assert.Equal(Staging, actualEnv)
+			})
+		}
+	})
+
+	t.Run("should return environment 'develop' from variable 'CUSTOM_ENV' from any other value", func(t *testing.T) {
+		values := []string{
+			"develop",
+			"DEVELOP",
+			"ANY",
+		}
+
+		for _, value := range values {
+			value := value
+
+			t.Run(fmt.Sprintf("%s=%s", customVariableName, value), func(t *testing.T) {
+				assert := assert.New(t)
+
+				err := os.Setenv(customVariableName, value)
+				require.NoError(t, err)
+
+				actualEnv := GetEnvironmentFromVariable(customVariableName)
+				assert.Equal(Develop, actualEnv)
+			})
+		}
+	})
 }
